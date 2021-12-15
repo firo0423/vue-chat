db793591ec6ee3efe214444087b2420bf481d7e4
firo <cfiro@qq.com>
2021-12-15 17:46:39 +0800

    样式框架完成

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,84 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="background"></div>
+    <div class="shell">
+      <div class="aside">
+        <user />
+        <list />
+      </div>
+      <div class="main">
+        <chatarea />
+        <entertextarea />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import user from "./components/user.vue";
+import list from "./components/list.vue";
+import chatarea from "./components/chatarea.vue";
+import entertextarea from "./components/entertextarea.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    user,
+    list,
+    chatarea,
+    entertextarea,
+  },
+  data() {
+    return {};
+  },
+};
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.shell {
+  position: absolute;
+  z-index: 1;
+  width: 800px;
+  height: 600px;
+  overflow: hidden;
+  border-radius: 10px;
+  left: 50%;
+  transform: translate(-50%, 0%);
+  margin-top: 40px;
+}
+.background {
+  background: url("../src/assets/bg.jpg");
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  background-size: 100% 100%;
+  z-index: -1;
+}
+
+.aside,
+.main {
+  height: 100%;
+}
+.aside {
+  float: left;
+  color: #f4f4f4;
+  background-color: #2e3238;
+  width: 200px;
+}
+.main {
+  position: relative;
+  overflow: hidden;
+  background-color: #eee;
 }
 </style>
---
+++ src/components/user.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="user">
+    <header>
+      <img :src="userimg" alt="" class="avatar" />
+      <span class="name">李狗蛋</span>
+    </header>
+    <footer>
+      <input
+        class="search"
+        type="text"
+        v-model="$store.state.filterKey"
+        placeholder="search user..."
+      />
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "user",
+  data() {
+    return {
+      // 计划使用vuex来储存 先填着
+      userimg: require("../assets/1.jpg"),
+    };
+  },
+};
+</script>
+
+<style scoped>
+#card {
+  padding: 12px;
+}
+.avatar {
+  width: 40px;
+  height: 40px;
+  vertical-align: middle; /*这个是图片和文字居中对齐*/
+}
+.name {
+  display: inline-block;
+  padding: 10px;
+  margin-bottom: 15px;
+  font-size: 16px;
+}
+.search {
+  background: #26292e;
+  height: 30px;
+  line-height: 30px;
+  padding: 0 10px;
+  border: 1px solid #3a3a3a;
+  border-radius: 4px;
+  outline: none; /*鼠标点击后不会出现蓝色边框*/
+  color: #fff;
+}
+</style>
